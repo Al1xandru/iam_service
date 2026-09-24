6ac9020986f85fae85f9378d87a56d6ef497f726
Alex <al1xbodeanu@gmail.com>
2026-09-24 19:22:57 +0200

    primary-qualifier practice

--- src/main/java/com/post_hub/iam_service/service/impl/SecondCommentServiceImpl.java
+++ src/main/java/com/post_hub/iam_service/service/impl/SecondCommentServiceImpl.java
@@ -1,11 +1,13 @@
 package com.post_hub.iam_service.service.impl;
 
 import com.post_hub.iam_service.service.CommentService;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service("advancedCommentService")
 public class SecondCommentServiceImpl implements CommentService {
     private final List<String> comments = new ArrayList<>();
 
